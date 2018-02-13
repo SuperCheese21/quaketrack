2e52b9efbf53410ac2a2d80c5b67b53a7a241b63
SuperCheese21 <supercheese21@comcast.net>
2018-02-13 01:17:29 -0800

    Added background task code

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 import { AppRegistry } from 'react-native';
 import App from './src/components/App';
+import BackgroundFetch from 'react-native-background-fetch';
+
+let BackgroundTask = async (event) => {
+    console.log('- BackgroundFetch HeadlessTask start');
+    // Important:  await asychronous tasks when using HeadlessJS.
+    await doAction();
+    // Required:  Signal to native code that your task is complete.
+    // If you don't do this, your app could be terminated and/or assigned
+    // battery-blame for consuming too much time in background.
+    BackgroundFetch.finish();
+    console.log('- BackgroundFetch HeadlessTask finished');
+}
+
+// Simulate a long-running task (eg: HTTP request)
+function doAction() {
+    let timeout = 5000;
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log('*** TIMEOUT ***');
+            resolve();
+        }, timeout);
+    });
+}
 
 AppRegistry.registerComponent('quaketrack', () => App);
+AppRegistry.registerHeadlessTask('BackgroundFetch', () => BackgroundTask);
