d207a1f428b6c61ca1426da4019b31dd6a01e79d
SuperCheese21 <ethan.shields21@gmail.com>
2018-10-13 03:09:42 -0700

    Removed bluebird, replaced Promise.all with async/await

--- src/api/registerForPushNotificationsAsync.js
+++ src/api/registerForPushNotificationsAsync.js
@@ -2,33 +2,29 @@ import { Platform } from 'react-native';
 import { Constants, Location, Notifications, Permissions } from 'expo';
 import * as firebase from 'firebase';
 import moment from 'moment';
-import Promise from 'bluebird';
 
 /**
  * [registerForPushNotificationsAsync description]
  * @param  {[type]} uid [description]
  */
 export default async function registerForPushNotificationsAsync(uid) {
-    Promise.all([getPushToken(), getLocation()])
-        .then(res => {
-            firebase.database().ref('users').child(uid).update({
-                expoPushToken: res[0],
-                minMagnitude: 5,
-                notifications: true,
-                location: {
-                    coords: {
-                        latitude: res[1].coords.latitude,
-                        longitude: res[1].coords.longitude
-                    },
-                    accuracy: res[1].coords.accuracy,
-                    mocked: res[1].mocked
-                },
-                updated: res[1].timestamp
-            });
-        })
-        .catch(err => {
-            console.error(err);
-        });
+    const token = await getPushToken();
+    const location = await getLocation();
+
+    firebase.database().ref('users').child(uid).update({
+        expoPushToken: token,
+        minMagnitude: 5,
+        notifications: true,
+        location: {
+            coords: {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude
+            },
+            accuracy: location.coords.accuracy,
+            mocked: location.mocked
+        },
+        updated: location.timestamp
+    });
 }
 
 /**
@@ -49,6 +45,7 @@ async function getPushToken() {
     }
 
     const token = await Notifications.getExpoPushTokenAsync();
+
     return token;
 }
 
