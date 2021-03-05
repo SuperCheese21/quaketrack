b72e14c2cc86f2c614697a3dce54d94fb7318841
SuperCheese21 <ethan.shields21@gmail.com>
2021-03-05 00:39:21 -0800

    Refactored Notifications screen component

--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -1,18 +1,18 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { Button, Switch } from 'react-native-paper';
 
-import LoadingSpinner from '../components/LoadingSpinner';
-import SettingsContainer from '../components/SettingsContainer';
-import SettingsItem from '../components/SettingsItem';
-import SettingsItemLabel from '../components/SettingsItemLabel';
-import Slider from '../components/Slider';
 import {
   getNotificationSettings,
   updateNotificationSettings,
 } from '../api/firebase';
-import colors from '../config/colors.json';
+import LoadingSpinner from '../components/LoadingSpinner';
 import { QuakesContext } from '../components/QuakesProvider';
+import SettingsContainer from '../components/SettingsContainer';
+import SettingsItem from '../components/SettingsItem';
+import SettingsItemLabel from '../components/SettingsItemLabel';
+import Slider from '../components/Slider';
+import colors from '../config/colors.json';
 
 const styles = {
   settingsSliderValue: {
@@ -24,123 +24,96 @@ const styles = {
   },
 };
 
-class Notifications extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      settings: {},
-    };
-  }
+const Notifications = ({ navigation }) => {
+  const { uid } = useContext(QuakesContext);
 
-  async componentDidMount() {
-    const { uid } = this.context;
+  const [isLoading, setIsLoading] = useState(true);
+  const [settings, setSettings] = useState({});
+
+  const fetchSettings = useCallback(async () => {
     const {
       notifications,
       minMagnitude,
       updates,
     } = await getNotificationSettings(uid);
-    this.setState({
-      settings: {
-        notifications,
-        minMagnitude,
-        updates,
-      },
-      isLoading: false,
+    setSettings({
+      notifications,
+      minMagnitude,
+      updates,
     });
-  }
+    setIsLoading(false);
+  }, [uid]);
 
-  updateSettings = (property, value) => {
-    const { settings } = this.state;
-    const newSettings = {
-      ...settings,
+  useEffect(() => {
+    fetchSettings();
+  }, [fetchSettings]);
+
+  const updateSettings = (property, value) =>
+    setSettings(oldSettings => ({
+      ...oldSettings,
       [property]: value,
-    };
-    this.setState({ settings: newSettings });
-  };
+    }));
 
-  onSaveAndClose = async () => {
-    const { navigation } = this.props;
-    const { settings } = this.state;
-    const { uid } = this.context;
-
-    this.setState({ isLoading: true });
+  const onSaveAndClose = async () => {
+    setIsLoading(true);
     await updateNotificationSettings(uid, settings);
-    this.setState({ isLoading: false });
-
+    setIsLoading(false);
     navigation.goBack();
   };
 
-  render() {
-    const { navigation } = this.props;
-    const { isLoading } = this.state;
-
-    if (isLoading) {
-      return <LoadingSpinner />;
-    }
-
-    const { settings } = this.state;
-
-    return (
-      <SettingsContainer>
-        <SettingsItem>
-          <SettingsItemLabel text="Notifications" />
-          <Switch
-            value={settings.notifications}
-            onValueChange={value => this.updateSettings('notifications', value)}
-            color={colors.accent}
-          />
-        </SettingsItem>
-
-        <SettingsItem disabled={!settings.notifications}>
-          <SettingsItemLabel text="Minimum Magnitude" />
-          <Text style={styles.settingsSliderValue}>
-            {settings.minMagnitude}
-          </Text>
-        </SettingsItem>
-
-        <Slider
-          disabled={!settings.notifications}
-          label="Minimum Magnitude"
-          minimumValue={2}
-          maximumValue={8}
-          step={0.5}
-          value={settings.minMagnitude}
-          onValueChange={value => this.updateSettings('minMagnitude', value)}
-        />
-
-        <SettingsItem disabled={!settings.notifications}>
-          <SettingsItemLabel text="Updates" />
-          <Switch
-            value={settings.updates}
-            onValueChange={value => this.updateSettings('updates', value)}
-            color={colors.accent}
-          />
-        </SettingsItem>
-
-        <SettingsItem
-          style={{ height: 'auto', justifyContent: 'space-around' }}
-        >
-          <Button
-            mode="contained"
-            color="red"
-            onPress={() => navigation.goBack()}
-          >
-            Cancel
-          </Button>
-          <Button
-            mode="contained"
-            color={colors.accent}
-            onPress={this.onSaveAndClose}
-          >
-            Save & Close
-          </Button>
-        </SettingsItem>
-      </SettingsContainer>
-    );
+  if (isLoading) {
+    return <LoadingSpinner />;
   }
-}
 
-Notifications.contextType = QuakesContext;
+  return (
+    <SettingsContainer>
+      <SettingsItem>
+        <SettingsItemLabel text="Notifications" />
+        <Switch
+          value={settings.notifications}
+          onValueChange={value => updateSettings('notifications', value)}
+          color={colors.accent}
+        />
+      </SettingsItem>
+
+      <SettingsItem disabled={!settings.notifications}>
+        <SettingsItemLabel text="Minimum Magnitude" />
+        <Text style={styles.settingsSliderValue}>{settings.minMagnitude}</Text>
+      </SettingsItem>
+
+      <Slider
+        disabled={!settings.notifications}
+        label="Minimum Magnitude"
+        minimumValue={2}
+        maximumValue={8}
+        step={0.5}
+        value={settings.minMagnitude}
+        onValueChange={value => updateSettings('minMagnitude', value)}
+      />
+
+      <SettingsItem disabled={!settings.notifications}>
+        <SettingsItemLabel text="Updates" />
+        <Switch
+          value={settings.updates}
+          onValueChange={value => updateSettings('updates', value)}
+          color={colors.accent}
+        />
+      </SettingsItem>
+
+      <SettingsItem style={{ height: 'auto', justifyContent: 'space-around' }}>
+        <Button
+          mode="contained"
+          color="red"
+          onPress={() => navigation.goBack()}
+        >
+          Cancel
+        </Button>
+        <Button mode="contained" color={colors.accent} onPress={onSaveAndClose}>
+          Save & Close
+        </Button>
+      </SettingsItem>
+    </SettingsContainer>
+  );
+};
 
 export default Notifications;
